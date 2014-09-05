56b1142465df4318ff3f2c34cc19306c58987be5
Dima Sukhikh <coddeys@gmail.com>
2014-09-05 18:34:20 +0400

    Add berkshelf

--- cookbooks/webserver/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'webserver'
-maintainer       ''
-maintainer_email ''
-license          ''
-description      'Installs/Configures webserver'
-long_description 'Installs/Configures webserver'
-version          '0.1.0'
-
---
+++ uni/metadata.rb
@@ -0,0 +1,8 @@
+name             'uni'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures uni'
+long_description 'Installs/Configures uni'
+version          '0.1.0'
+
