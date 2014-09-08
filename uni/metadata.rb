84c67c91ddf5bc4afa25b529f5b902768af825a1
Dima Sukhikh <coddeys@gmail.com>
2014-09-08 18:06:56 +0400

    Delete berkshelf cookbooks

--- uni/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'uni'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures uni'
-long_description 'Installs/Configures uni'
-version          '0.1.0'
-
