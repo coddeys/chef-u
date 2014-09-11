394debae15153d06cef2671d8086dca3cf94c680
Dima Sukhikh <coddeys@gmail.com>
2014-09-11 17:06:22 +0400

    add depends application ruby

---
+++ cookbooks/rails-demo/metadata.rb
@@ -0,0 +1,10 @@
+name             'rails-demo'
+maintainer       'coddeys'
+maintainer_email 'coddeys@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures rails-demo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+depends 'application'
+depends 'application_ruby'
+
