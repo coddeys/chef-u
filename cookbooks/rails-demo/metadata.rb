cc8a8469311dd57587396c10d88019eb1ab48ecd
Dima Sukhikh <coddeys@gmail.com>
2014-09-11 18:38:13 +0400

    Add rails demo

---
+++ cookbooks/rails-demo/metadata.rb
@@ -0,0 +1,8 @@
+name             'rails-demo'
+maintainer       'coddeys'
+maintainer_email 'coddeys@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures rails-demo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
---
+++ cookbooks/rails-demo/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: rails-demo
+# Recipe:: default
+#
+# Copyright 2014, Coddeys
+#
+# All rights reserved - Do Not Redistribute
+#
+application "rails-demo" do
+  path "/var/www/rails-apps/rails-demo"
+  owner "vagrant"
+  group "vagrant"
+  repository "https://github.com/rails/rails.git"
+end
